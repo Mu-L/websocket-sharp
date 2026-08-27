ce7b459282b73a5882a9d17056cac02fdc51b03a
sta <sta.blockhead@gmail.com>
2026-08-27 23:36:47 +0900

    [Modify] Edit it

--- websocket-sharp/Server/WebSocketServer.cs
+++ websocket-sharp/Server/WebSocketServer.cs
@@ -530,7 +530,7 @@ namespace WebSocketSharp.Server
     /// configuration used to provide a secure connection.
     /// </value>
     /// <exception cref="InvalidOperationException">
-    /// The server does not provide secure connections.
+    /// The server does not provide a secure connection.
     /// </exception>
     public ServerSslConfiguration SslConfiguration {
       get {
