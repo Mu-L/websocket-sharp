af0b5e74dc81321e951abaa7e3c2637737ed5382
sta <sta.blockhead@gmail.com>
2026-09-02 23:44:47 +0900

    [Modify] Throw it

--- websocket-sharp/Server/WebSocketServer.cs
+++ websocket-sharp/Server/WebSocketServer.cs
@@ -516,8 +516,11 @@ namespace WebSocketSharp.Server
 
       set {
         lock (_sync) {
-          if (!canSet ())
-            return;
+          if (!canSet ()) {
+            var msg = "The current state of the server is neither Ready nor Stop.";
+
+            throw new InvalidOperationException (msg);
+          }
 
           _reuseAddress = value;
         }
